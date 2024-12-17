1356dbdc02fd697ecdae36611f500f49b659e1dd
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2024-12-18 01:52:31 +0600

    double equal and tripple equal

---
+++ equal.js
@@ -0,0 +1,78 @@
+// const first =2
+// const second='2'
+
+// if(first==second){
+//      console.log('values are equal')
+// }
+// else{
+//      console.log('values are not equal')
+// }
+
+// if(first===second){
+//     console.log('values are equal')
+// }
+// else{
+//     console.log('values are not equal')
+// }
+
+//  == just check value check
+//  === check both value and type
+
+// const first = 1;
+// const second = true;
+
+// if (first == second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+// if (first === second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+// behind the seen == kicu kahini ghotay
+// type coercion
+// type conversion
+// type casting
+
+// == ,=== do works in primitive
+//  in non primitive they check reference
+
+
+// const first = [];
+// const second = [];
+
+// if (first == second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+// if (first === second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+// same reference
+// const first = [];
+// const second = first;
+
+// if (first == second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+// if (first === second) {
+//   console.log("values are equal");
+// } else {
+//   console.log("values are not equal");
+// }
+
+
+
+// how to compare  to obj or array using looping ?
