061ae1572e30ca6f45dd4cb2020a48b890c711bc
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2025-01-10 23:35:53 +0600

    const and let discussion

---
+++ variable.js
@@ -0,0 +1,7 @@
+// var - globally declare we should not use this
+
+// let and const are blocked scoped but the difference is we cant change val ue which we declare with const but we can change the value or re assign new value of that variable which we declared with let
+
+// While let and const are block-scoped, var declarations are either globally scoped or function-scoped. Let variables can be updated but not re-declared, const variables cannot be updated or re-declared, and var variables may both be updated and re-declared inside their scope.
+
+// 'const' Function Expressions: When defining functions using 'const', it creates a function expression. Unlike 'function' declarations, 'const' function expressions are not hoisted and result in an error when called before declaration.
